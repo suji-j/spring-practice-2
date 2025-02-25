056900407e612e628d7185eede7be8a2b96f2449
SUJI-J <suzy852@naver.com>
2025-02-25 20:46:14 +0900

    style: 주석 제거

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -15,15 +15,12 @@ public class SingletonTest {
     void pureContainer() {
         AppConfig appConfig = new AppConfig();
 
-        // 1. 조회 :  호출할 때 마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
 
-        // 참조 값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        // memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
     }
 
